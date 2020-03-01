e7046e54b4253efb5429802b62ed5906848a3551
JongwonYoun <jeffyoun@naver.com>
2020-03-01 17:53:11 +0900

    수정된거에 맞게 ictCAU 수정

--- simpleCrawling/src/ictCAU.js
+++ simpleCrawling/src/ictCAU.js
@@ -1,40 +1,42 @@
 const getArticle = require("../lib/api");
-const { save, read } = require("../lib/db");
+const {
+  save,
+  read
+} = require("../lib/db");
 const IctCAU = require("../models/ictCAU");
 
 module.exports.saveNotice = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   return getArticle(
-    [
-      {
-        url: "https://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php",
-        trSelector: "body > div > div.content > div > table > tbody > tr",
-        dataFunc: $ => {
-          const title = $(this)
-            .find(".cont > a")
-            .text();
-          const link = `https://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php?cmd=view&cpage=1&idx=${$(
+    [{
+      url: "https://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php",
+      trSelector: "body > div > div.content > div > table > tbody > tr",
+      dataFunc: $ => {
+        const title = $(this)
+          .find(".cont > a")
+          .text();
+        const link = `https://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php?cmd=view&cpage=1&idx=${$(
             this
           )
             .find(".cont > a")
             .prop("href")
             .match(/\d+/)}&search_gbn=1&search_keyword=`;
-          const date = $(this)
-            .find("td:nth-child(3)")
-            .text();
-          return {
-            title,
-            link,
-            date
-          };
-        }
+        const date = $(this)
+          .find("td:nth-child(3)")
+          .text();
+        return {
+          title,
+          link,
+          date
+        };
       }
-    ],
+    }],
     save(IctCAU),
     callback
   );
 };
-module.exports.readNotice = async (event, context, callback) => {
-  return read(IctCAU, callback);
-
-  read()
+module.exports.readNotice = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  response = await read(IctCAU);
+  return response;
 };
