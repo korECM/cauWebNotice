3ce64c1841331d5fc488e87e2a971724b479e24f
JongwonYoun <jeffyoun@naver.com>
2020-03-05 03:04:46 +0900

    CORS 대응

--- simpleCrawling/lib/utils.js
+++ simpleCrawling/lib/utils.js
@@ -1,6 +1,10 @@
 const createResponse = (status, body) => {
   const result = {
     statusCode: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
     body: JSON.stringify(body)
   };
   return result;
